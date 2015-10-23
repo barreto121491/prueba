41e3866fc9008c9f33e2193f6c549f48f0a83f97
Arturo Barreto Villaf√°n <Barreto@mbp-de-arturo.lan>
2015-10-23 15:29:12 -0500

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Arturo Barreto Villafán on 23/10/15.
+//  Copyright © 2015 Arturo Barreto Villafán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
