f3e9861ce15fcc4498fa2ead66879cc54ff893ff
Arturo Barreto Villaf√°n <Barreto@mbp-de-arturo.lan>
2015-10-23 15:47:22 -0500

    modificación 1

--- pruebaGit/ViewController.swift
+++ pruebaGit/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // Modificacion 1 prueba de git con xcode 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
